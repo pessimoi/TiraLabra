37712eb3e0a29d0c80e1b3759518c7f6a04ca8c4
Pessi <pessi.moilanen@helsinki.fi>
2012-08-09 14:36:57 +0300

    MapCreatorin JUNIT testit

---
+++ test/tiralabra/MapCreatorTest.java
@@ -0,0 +1,68 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tiralabra;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import org.junit.Before;
+
+/**
+ *
+ * @author admin
+ */
+public class MapCreatorTest {
+
+    private MapCreator mapCreator;
+    private int height, width, empty;
+
+    @Before
+    public void setUpClass() throws Exception {
+
+        height = 3;
+        width = 4;
+        empty = 100;
+        mapCreator = new MapCreator(height, width, empty);
+    }
+
+    @Test
+    public void get_empty_as_set_on_constructor() {
+        int amount = 100;
+        assertEquals(amount, mapCreator.getEmpty());
+    }
+
+    @Test
+    public void set_empty_changes_empty() {
+        int amount = 200;
+        mapCreator.setEmpty(amount);
+        assertEquals(amount, mapCreator.getEmpty());
+    }
+
+    @Test
+    public void get_height_as_set_on_constructor() {
+        int h = 3;
+        assertEquals(h, mapCreator.getHeight());
+    }
+
+    @Test
+    public void set_height_changes_height() {
+        int h = 6;
+        mapCreator.setHeight(h);
+        assertEquals(h, mapCreator.getHeight());
+    }
+
+    @Test
+    public void get_width_as_set_on_constructor(){
+        int w = 4;
+        assertEquals(w, mapCreator.getWidth());
+    }
+
+    @Test
+    public void set_width_changes_width(){
+        int w = 8;
+        mapCreator.setWidth(w);
+        assertEquals(w, mapCreator.getWidth());
+    }
+    
+}
